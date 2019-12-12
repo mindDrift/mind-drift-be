12d5fa50204c43e43235c5af1591c4fe31188214
Abbey Masters <abbeymasters@Abbeys-MBP.localdomain>
2019-12-12 14:15:17 -0800

    all sessions test passing

--- e2e/_tests_/sessions.test.js
+++ e2e/_tests_/sessions.test.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
-const request = require("../request");
-const connect = require("../../lib/utils/connect");
-const mongoose = require("mongoose");
+require('dotenv').config();
+const request = require('../request');
+const connect = require('../../lib/utils/connect');
+const mongoose = require('mongoose');
 
-describe("Settings API", () => {
+describe('Sessions', () => {
   beforeAll(() => {
     connect();
   });
@@ -19,11 +19,11 @@ describe("Settings API", () => {
   const session = {
     start: new Date(),
     duration: 16,
-    userId: "123456",
+    userId: '123456',
     settings: {
-      userId: "123456",
-      title: "Different Breathing Type",
-      description: "This is a different breathing technique.",
+      userId: '123456',
+      title: 'Different Breathing Type',
+      description: 'This is a different breathing technique.',
       inhale: 3,
       holdIn: 4,
       exhale: 5,
@@ -35,11 +35,11 @@ describe("Settings API", () => {
   const session2 = {
     start: new Date(),
     duration: 5,
-    userId: "123456",
+    userId: '123456',
     settings: {
-      userId: "123456",
-      title: "Another Breathing Pattern",
-      description: "This is a new breathing technique.",
+      userId: '123456',
+      title: 'Another Breathing Pattern',
+      description: 'This is a new breathing technique.',
       inhale: 1,
       holdIn: 5,
       exhale: 8,
@@ -50,15 +50,15 @@ describe("Settings API", () => {
 
   function postSessions(sessions) {
     return request
-      .post("/api/v1/sessions")
+      .post('/api/v1/sessions')
       .send(sessions)
       .expect(200)
       .then(({ body }) => body);
   }
 
-  it("posts sessions", () => {
+  it('posts sessions', () => {
     return request
-      .post("/api/v1/sessions")
+      .post('/api/v1/sessions')
       .send(session)
       .expect(200)
       .then(({ body }) => {
@@ -92,10 +92,10 @@ describe("Settings API", () => {
       });
   });
 
-  it("gets a session", () => {
+  it('gets a session', () => {
     return postSessions(session)
       .then(() => {
-        return request.get("/api/v1/sessions?userId=123456").expect(200);
+        return request.get('/api/v1/sessions?userId=123456').expect(200);
       })
       .then(({ body }) => {
         expect(body.length).toBe(1);
@@ -103,53 +103,42 @@ describe("Settings API", () => {
       });
   });
 
-  it("gets the last session", () => {
+  it('gets the last session', () => {
     return Promise.all([postSessions(session), postSessions(session2)])
       .then(() => {
-        return request.get("/api/v1/sessions?userId=123456").expect(200);
+        return request
+          .get('/api/v1/sessions/lastsession?userId=123456')
+          .expect(200);
       })
       .then(({ body }) => {
         console.log(body);
-        expect(body.length).toBe(2);
-        expect(body[1].duration).toBe(5);
-        expect(body).toMatchInlineSnapshot(`
-          Array [
-            Object {
-              "__v": 0,
-              "_id": "5df2b277a01ab0b6566f13d8",
-              "duration": 16,
-              "settings": Object {
-                "description": "This is a different breathing technique.",
-                "endTime": 16,
-                "exhale": 5,
-                "holdIn": 4,
-                "holdOut": 1,
-                "inhale": 3,
-                "title": "Different Breathing Type",
-                "userId": "123456",
-              },
-              "start": "2019-12-12T21:34:46.916Z",
+        expect(body).toMatchInlineSnapshot(
+          {
+            ...session,
+            __v: 0,
+            _id: expect.any(String),
+            start: expect.any(String)
+          },
+          `
+          Object {
+            "__v": 0,
+            "_id": Any<String>,
+            "duration": 16,
+            "settings": Object {
+              "description": "This is a different breathing technique.",
+              "endTime": 16,
+              "exhale": 5,
+              "holdIn": 4,
+              "holdOut": 1,
+              "inhale": 3,
+              "title": "Different Breathing Type",
               "userId": "123456",
             },
-            Object {
-              "__v": 0,
-              "_id": "5df2b277a01ab0b6566f13d9",
-              "duration": 5,
-              "settings": Object {
-                "description": "This is a new breathing technique.",
-                "endTime": 20,
-                "exhale": 8,
-                "holdIn": 5,
-                "holdOut": 4,
-                "inhale": 1,
-                "title": "Another Breathing Pattern",
-                "userId": "123456",
-              },
-              "start": "2019-12-12T21:34:46.916Z",
-              "userId": "123456",
-            },
-          ]
-        `);
+            "start": Any<String>,
+            "userId": "123456",
+          }
+        `
+        );
       });
   });
 });
